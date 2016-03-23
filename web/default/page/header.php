b1d5e8c6aa70362b05ae9c33e704d71a80f5b93a
Artthed <arthur.desoutter@outlook.com>
2016-03-23 12:01:01 +0100

    maj fancybox

--- web/default/page/header.php
+++ web/default/page/header.php
@@ -39,6 +39,18 @@ switch($dossier){
 <script src="../../vendor/jquery-2.2.1/jquery-migrate-1.2.1.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" type="text/javascript"></script>
 
+<link rel="stylesheet" type="text/css" href="../../vendor/fancybox/source/jquery.fancybox.css">
+	<script type="text/javascript" src="../../vendor/fancybox/source/jquery.fancybox.js"></script>
+	<script type="text/javascript" src="../../vendor/fancybox/source/jquery.fancybox.pack.js"></script>
+	<script type="text/javascript" src="../../vendor/fancybox/source/jquery.mousewhell.js"></script>
+
+	<script type="text/javascript">
+	$(document).ready(function() {
+		$(".fancybox").fancybox();
+	});
+	</script>
+
+
 <title><?php echo $titre ?></title>
 </head>
 
