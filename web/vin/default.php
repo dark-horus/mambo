8a9a25019dfa7007ab27ecfc93f60045f85d6ff8
Ratou <romane.viard@live.fr>
2016-04-01 12:07:55 +0200

    update

--- web/vin/default.php
+++ web/vin/default.php
@@ -1,10 +1,31 @@
 <?php
-include('../default/page/header.php');
+include('../default/page/start_header.php');
 ?>
-<!-- Place en dessous ton contenu + tes liens vers CSS et JS ! -->
+<!-- Place en dessous ton CSS dans Vendor, et ton CSS local -->
+
+
+<!-- fin CSS -->
+<?php include('../default/page/header.php'); ?>
+
+<!-- Place ici tes scripts Javascripts dans Vendor, et ton JS local -->
 
 
 
+<!-- Fin Scripts -->
+
+
+<?php include('../default/page/end_header.php'); ?>
+
+<!-- Place ici ton contenu -->
+
+<!--img src="img/infographie2.jpg" width="100"-->
+<style> 
+	body{
+		background-image:url(img/pagevinweb.jpg);
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	</style>
 
 
 
