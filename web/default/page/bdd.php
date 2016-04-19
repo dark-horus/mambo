ca0f75c5c5201ae6c4254e2d3850a3754047c397
Artthed <arthur.desoutter@outlook.com>
2016-04-20 00:01:55 +0200

    chemin bdd

--- web/default/page/bdd.php
+++ web/default/page/bdd.php
@@ -4,5 +4,6 @@ try {
     $bdd = new PDO( 'mysql:host=localhost;dbname=mambo;charset=utf8', 'root', '' );
 } catch ( Exception $e ) {
     // Merci de mettre le fichier sql permettant de créer la bdd pour ceux qui ne l'ont pas ;)
+	// Le fichier sql est placé dans le chemin suivant : web/default/mambo.sql
 //    die( 'Erreur : ' . $e->getMessage() );
 }
