75ae6fb45c6aa7200cadcacea198c2e45ec8d375
Artthed <arthur.desoutter@outlook.com>
2016-04-21 23:36:59 +0200

    immeuble validation etape+ image map
    image map livre, stylo et appareil photo
    validation de l'étape

--- web/immeuble/henri.php
+++ web/immeuble/henri.php
@@ -2,7 +2,7 @@
 include('../default/page/start_header.php');
 ?>
 <!-- Place en dessous ton CSS dans Vendor, et ton CSS local -->
-
+<link rel="stylesheet" type="text/css" href="style.css">
 
 <!-- fin CSS -->
 <?php include('../default/page/header.php'); ?>
@@ -17,11 +17,75 @@ include('../default/page/start_header.php');
 <?php include('../default/page/end_header.php'); ?>
 
 <!-- Place ici ton contenu -->
-<img class="img img-responsive" src="images/henri.png" width="100%">
+<div id="image">
+<img class="img-responsive" src="images/henriHD.png" width="100%" usemap="#maparthur">
+</div>
+ <map name="maparthur" id="maparthur">
+		<area shape="rect" title="livre" coords="2,804,96,850" href="#" onclick="showElem()"/>  		
+        <area shape="rect" coords="1263,944,1298,960" href="#" title="appareil" onclick="showElem2()"/>
+     	<area shape="rect" coords="1846,746,1888,767" href="#" title="Stylo" onclick="showElem3()"/>
 
+ </map>
 
+ <div id="obj">
+ <h2>Objets trouvés :</h2>
+ <ul class="list-inline">
+	<li id="livre" style="visibility:hidden;"><img class="img-responsive" src="images/livre.png"></li>
+	<li id="appareil" style="visibility:hidden;"><img class="img-responsive" src="images/livre.png"></li>
+	<li id="stylo" style="visibility:hidden;"><img class="img-responsive" src="images/livre.png"></li>
+ </ul>
+   </div>
+ 
+ <div id="question" style="visibility:hidden">
+ <p>Avez vous deviné à qui appartient cette chambre ?</p>
+ <form action="verif.php" method="post" name="param" onsubmit="return confirmation()">
 
+<input type="radio" name="henri" id="r1" value="Alejandro" onclick="reponse()">
+Alejandro
+<input type="radio" name="henri" id="r2" value="Henri" onclick="reponse()">
+Henri
+<input type="radio" name="henri" id="r3" value="Simao" onclick="reponse()">
+Simao
+<input type="submit" name="valider" id="valider" value="Valider l'étape">
 
+</form>
+</div>
+   
+   
+   
+<script>
+	var count = 0;
+function showElem() {
+    document.getElementById("livre").style.visibility = "visible";
+	count = count + 1;
+	if (count > 2) {
+		document.getElementById("question").style.visibility = "visible";
+	}
+}
+function showElem2() {
+    document.getElementById("appareil").style.visibility = "visible";
+	count = count + 1;
+		if (count > 2) { 	
+			document.getElementById("question").style.visibility = "visible";
+		}
+}
+function showElem3() {
+    document.getElementById("stylo").style.visibility = "visible"; 
+	count = count + 1;
+		if (count > 2) {
+			document.getElementById("question").style.visibility = "visible";
+		}
+}
+</script>
+<?php 
+if(isset($_SESSION['badanswer'])){
+	?>
+	<script>
+	alert("Mauvaise réponse !");
+	</script>
+	<?php
+	}
+?>
 <!-- Fin du contenu -->
 <?php 
 include('../default/page/footer.php');
---
+++ web/immeuble/verif.php
@@ -0,0 +1,89 @@
+<?php
+session_start();
+if (!isset($_SESSION['id'])){
+	header('location: ../../index.php');
+}
+else {
+$password = $_SESSION['id'];
+//connexion à la bdd
+include('../default/page/bdd.php');
+
+//Récupération des informations de l'utilisateur
+$login = $bdd->query('SELECT * FROM `user` WHERE password = "'.$password.'"');
+$res = $login->fetch();
+
+//variables
+$id = $res['iduser'];
+	
+
+	
+if (isset($_POST['henri']))
+{
+	$page = "henri.php";
+	if($_POST['henri'] == "Henri"){
+		$validate = "true";				
+	}
+	else {
+		$validate = "false";
+	}
+}
+
+
+if (isset($_POST['alejandro']))
+{
+	$page = "alejandro.php";
+	if($_POST['alejandro'] == "Alejandro"){
+		$validate = "true";				
+	}
+	else {
+		$validate = "false";
+	}
+}
+if (isset($_POST['simao']))
+{
+	$page = "simao.php";
+	if($_POST['simao'] == "Simao"){
+		$validate = "true";				
+	}
+	else {
+		$validate = "false";
+	}
+}
+
+	//Inscription dans la bdd si résultat bon
+	echo $validate;
+	if($validate == "true"){
+		$req = $bdd->prepare('INSERT INTO `etape` (user_ID, etape_ID, field, value) VALUES(:user_ID, :etape_ID, :field, :value)');
+		$req->execute(array(
+		'user_ID' => $id,
+		'etape_ID' => "Immeuble",
+		'field' => "",
+		'value' => ""
+		));
+		//Etape en cours :
+		$req2 = "UPDATE `user` SET current_etape = 'Immeuble' WHERE iduser = '$id'";
+		$req2 = $bdd->exec($req2);
+
+		//comptabilise pour la progression
+
+		$requete = $bdd->query('SELECT etape_ID FROM `etape` WHERE user_ID = "'.$id.'" GROUP BY etape_ID');
+		$progression = 0;
+		while($donnees = $requete->fetch())
+		{
+			$progression = $progression+1;
+		}
+
+		$req3 = "UPDATE `user` SET progression = '$progression' WHERE iduser = '$id'";
+		$req3 = $bdd->exec($req3);
+		if(isset($_SESSION['badanswer'])){
+			unset($_SESSION['badanswer']);
+		}
+		header('location: '.$page);
+		
+	}
+	else{
+		$_SESSION['badanswer'] = "false";
+		header('location: '.$page);
+	}
+}
+?>
