358e500949b0677fae50612bc1678bdc816c323b
Ratou <romane.viard@live.fr>
2016-04-21 15:14:29 +0200

    ajout télécharger

--- web/vin/index.php
+++ web/vin/index.php
@@ -9,17 +9,18 @@ include('../default/page/start_header.php');
 
 <!-- Place ici tes scripts Javascripts dans Vendor, et ton JS local -->
 
-
-
 <!-- Fin Scripts -->
-
 
 <?php include('../default/page/end_header.php'); ?>
 
 <!-- Place ici ton contenu -->
 
-	
-<img src="img/pagevinweb.jpg" width="100%">
+	<img src="img/pagevinweb2.jpg" width="100%" >
+
+	<a href="http://localhost/mambo/web/vin/img/pagevinweb2.jpg"  download="Infographie_Mambo_Canaille"> 
+	<img src="img/telecharger.png" width="200px"> </a>
+ 
+ 
  
  <!-- bouton valider + enregistrement bdd -->
  <form action="../default/page/recEtape.php" method="post">
