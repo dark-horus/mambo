003861e86dd2076e769c7b32962b8c8ed2115a35
Artthed <arthur.desoutter@outlook.com>
2016-04-22 14:38:42 +0200

    pages erreurs

---
+++ web/404.php
@@ -0,0 +1,58 @@
+
+<style>
+h1 {
+text-align: center;
+font-size: 35px;
+font-family:'Special Elite';
+color:#35201e;
+margin-top:150px;
+font-weight: normal;
+font-style: normal;
+}
+
+body {
+  background-image: url(img/fondlightbox.png);
+  margin:0;
+  padding:0;
+  background-position:center;
+  background-repeat: no-repeat; 
+  background-attachment:scroll;
+  -webkit-background-size: cover;
+  background-size: cover; 
+  border-color: blue;
+ 
+}    
+
+
+h2 {
+    text-align: center;
+    font-size: 18px;
+    font-family:'Special Elite';
+    color:black;
+    margin-top:10px;
+	line-height:25px
+	
+}
+
+
+.bouton {
+	width:80%;
+	
+	
+}
+
+
+    
+</style>
+<meta charset="utf-8">
+<body>
+<h1> Oups! Une erreur est survenue.<h1>
+
+<h2> La page que vous avez demandée n’a pas été trouvée.<br/> 
+Il se peut que le lien que vous avez utilisé soit rompu ou que vous ayez tapé l’adresse (URL) incorrectement.<br/>
+Essayez de rafraichir la page en appuyant sur F5.<br/>
+Ou retournez en page d'accueil pour profiter de l'experience Mambo Canaille! </h2>
+<center><a href="http://localhost/mambo/web/accueil/" > <img src="img/retour.png"></a></center>
+
+</body>
+
