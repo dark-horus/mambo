ddb32f89855ba6cb5be501fe3f4e7803e4428ee4
kieranpaccoud <kieran.paccoud@gmail.com>
2016-04-22 12:30:11 +0200

    Alcalay_Etape

--- web/chien/default.php
+++ web/chien/default.php
@@ -18,27 +18,65 @@ include('../default/page/start_header.php');
 
 <!-- Place ici ton contenu -->
 <style type="text/css">
+
+#slider_chien{
+
+
+	
+}
+
 #slider1{
 
-height: 200px;
+height: auto;
 margin-top: 100px;
 
 }
+#slider2{
+
+height: 200px;
+margin-top:15px;
+
+}
+#slider3{
+
+height: 200px;
+margin-top:15px;
+
+}
+#slider4{
+
+height: 170px;
+margin-top:15px;
+
+}
+#slider5{
+
+height: 200px;
+margin-top:15px;
+
+}
+
 #body_fictif{
 
 
 height: 1080px;
 width: 100%;
 margin: 0px;
-background-image: url("img/fond.png");
-
+background-image: url("img/fond.jpg");
+background-size: cover;
+background-repeat: no-repeat;
 
 }
+
 
 
 </style>
 
 <div class="container" id="body_fictif">
+
+
+<!-- SLIDER DES TETE-->
+<div class="col-md-12" id="slider_chien">
 	<!--Cheveux-->
 	<div class="col-md-12" id="slider1">
 		<div id="cheveux" class="carousel slide" data-ride="carousel">
@@ -48,73 +86,299 @@ background-image: url("img/fond.png");
 		    <li data-target="#cheveux" data-slide-to="1"></li>
 		    <li data-target="#cheveux" data-slide-to="2"></li>
 		    <li data-target="#cheveux" data-slide-to="3"></li>
+		    <li data-target="#cheveux" data-slide-to="4"></li>
+		    <li data-target="#cheveux" data-slide-to="5"></li>
+		    <li data-target="#cheveux" data-slide-to="6"></li>
+		    <li data-target="#cheveux" data-slide-to="7"></li>
 		  </ol>
 
 		  <!-- Wrapper for slides -->
 		  <div class="carousel-inner" role="listbox">
 		    <div class="item active">
-		      <img src="img/alejandro.png" alt="alejandro">
+		      <img src="img/Cheveux/C_1.jpg" alt="C_1">
 		    </div>
 
 		    <div class="item">
-		      <img src="img/henry.png" alt="henry">
+		      <img src="img/Cheveux/C_2.jpg" alt="C_2">
 		    </div>
 
 		    <div class="item">
-		      <img src="img/simao.png" alt="siamo">
+		      <img src="img/Cheveux/C_3.jpg" alt="C_3">
+		    </div>
+		    <div class="item active">
+		      <img src="img/Cheveux/C_4.jpg" alt="C_4">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Cheveux/C_5.jpg" alt="C_5">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Cheveux/C_6.jpg" alt="C_6">
+		    </div>
+		    <div class="item">
+		      <img src="img/Cheveux/C_7.jpg" alt="C_7">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Cheveux/C_8.jpg" alt="C_8">
 		    </div>
 		  </div>
 
 		  <!-- Left and right controls -->
 		  <a class="left carousel-control" href="#cheveux" role="button" data-slide="prev">
 		    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-		    <span class="sr-only">Previous</span>
+		    <span class="sr-only">Precedent</span>
 		  </a>
 		  <a class="right carousel-control" href="#cheveux" role="button" data-slide="next">
 		    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-		    <span class="sr-only">Next</span>
+		    <span class="sr-only">Suivant</span>
 		  </a>
 		</div>
 	</div>
 
-
-	<!--yeux slider-->
-	<div class="col-md-12" id="slider1">
-		<div id="yeux" class="carousel slide" data-ride="carousel">
+<!--Yeux-->
+	<div class="col-md-12" id="slider2">
+		<div id="Yeux" class="carousel slide" data-ride="carousel">
 		  <!-- Indicators -->
 		  <ol class="carousel-indicators">
-		    <li data-target="#yeux" data-slide-to="0"></li>
-		    <li data-target="#yeux" data-slide-to="1"></li>
-		    <li data-target="#yeux" data-slide-to="2"></li>
-		    <li data-target="#yeux" data-slide-to="3"></li>
+		    <li data-target="#Yeux" data-slide-to="0"></li>
+		    <li data-target="#Yeux" data-slide-to="1"></li>
+		    <li data-target="#Yeux" data-slide-to="2"></li>
+		    <li data-target="#Yeux" data-slide-to="3"></li>
+		    <li data-target="#Yeux" data-slide-to="4"></li>
+		    <li data-target="#Yeux" data-slide-to="5"></li>
+		    <li data-target="#Yeux" data-slide-to="6"></li>
+		    <li data-target="#Yeux" data-slide-to="7"></li>
 		  </ol>
 
 		  <!-- Wrapper for slides -->
 		  <div class="carousel-inner" role="listbox">
 		    <div class="item active">
-		      <img src="img/alejandro.png" alt="alejandro">
+		      <img src="img/Yeux/Y_1.jpg" alt="Y_1">
 		    </div>
 
 		    <div class="item">
-		      <img src="img/henry.png" alt="henry">
+		      <img src="img/Yeux/Y_2.jpg" alt="Y_2">
 		    </div>
 
 		    <div class="item">
-		      <img src="img/simao.png" alt="siamo">
+		      <img src="img/Yeux/Y_3.jpg" alt="Y_3">
+		    </div>
+		    <div class="item">
+		      <img src="img/Yeux/Y_4.jpg" alt="Y_4">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Yeux/Y_5.jpg" alt="Y_5">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Yeux/Y_6.jpg" alt="Y_6">
+		    </div>
+		    <div class="item">
+		      <img src="img/Yeux/Y_7.jpg" alt="Y_7">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Yeux/Y_8.jpg" alt="C_8">
 		    </div>
 		  </div>
 
 		  <!-- Left and right controls -->
-		  <a class="left carousel-control" href="#yeux" role="button" data-slide="prev">
+		  <a class="left carousel-control" href="#Yeux" role="button" data-slide="prev">
 		    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-		    <span class="sr-only">Previous</span>
+		    <span class="sr-only">Precedent</span>
 		  </a>
-		  <a class="right carousel-control" href="#yeux" role="button" data-slide="next">
+		  <a class="right carousel-control" href="#Yeux" role="button" data-slide="next">
 		    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-		    <span class="sr-only">Next</span>
+		    <span class="sr-only">Suivant</span>
 		  </a>
 		</div>
 	</div>
+
+<!--Nez-->
+	<div class="col-md-12" id="slider3">
+		<div id="Nez" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators">
+		    <li data-target="#Nez" data-slide-to="0"></li>
+		    <li data-target="#Nez" data-slide-to="1"></li>
+		    <li data-target="#Nez" data-slide-to="2"></li>
+		    <li data-target="#Nez" data-slide-to="3"></li>
+		    <li data-target="#Nez" data-slide-to="4"></li>
+		    <li data-target="#Nez" data-slide-to="5"></li>
+		    <li data-target="#Nez" data-slide-to="6"></li>
+		    <li data-target="#Nez" data-slide-to="7"></li>
+		  </ol>
+
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner" role="listbox">
+		    <div class="item active">
+		      <img src="img/Nez/N_1.jpg" alt="N_1">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Nez/N_2.jpg" alt="N_2">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Nez/N_3.jpg" alt="N_3">
+		    </div>
+		    <div class="item">
+		      <img src="img/Nez/N_4.jpg" alt="N_4">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Nez/N_5.jpg" alt="N_5">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Nez/N_6.jpg" alt="N_6">
+		    </div>
+		    <div class="item">
+		      <img src="img/Nez/N_7.jpg" alt="N_7">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Nez/N_8.jpg" alt="N_8">
+		    </div>
+		  </div>
+
+		  <!-- Left and right controls -->
+		  <a class="left carousel-control" href="#Nez" role="button" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		    <span class="sr-only">Precedent</span>
+		  </a>
+		  <a class="right carousel-control" href="#Nez" role="button" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		    <span class="sr-only">Suivant</span>
+		  </a>
+		</div>
+	</div>
+
+	<!--Bouche-->
+	<div class="col-md-12" id="slider4">
+		<div id="Bouche" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators">
+		    <li data-target="#Bouche" data-slide-to="0"></li>
+		    <li data-target="#Bouche" data-slide-to="1"></li>
+		    <li data-target="#Bouche" data-slide-to="2"></li>
+		    <li data-target="#Bouche" data-slide-to="3"></li>
+		    <li data-target="#Bouche" data-slide-to="4"></li>
+		    <li data-target="#Bouche" data-slide-to="5"></li>
+		    <li data-target="#Bouche" data-slide-to="6"></li>
+		    <li data-target="#Bouche" data-slide-to="7"></li>
+		  </ol>
+
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner" role="listbox">
+		    <div class="item active">
+		      <img src="img/Bouche/B_1.jpg" alt="B_1">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Bouche/B_2.jpg" alt="B_2">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Bouche/B_3.jpg" alt="B_3">
+		    </div>
+		    <div class="item">
+		      <img src="img/Bouche/B_4.jpg" alt="B_4">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Bouche/B_5.jpg" alt="B_5">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Bouche/B_6.jpg" alt="B_6">
+		    </div>
+		    <div class="item">
+		      <img src="img/Bouche/B_7.jpg" alt="B_7">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Bouche/B_8.jpg" alt="B_8">
+		    </div>
+		  </div>
+
+		  <!-- Left and right controls -->
+		  <a class="left carousel-control" href="#Bouche" role="button" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		    <span class="sr-only">Precedent</span>
+		  </a>
+		  <a class="right carousel-control" href="#Bouche" role="button" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		    <span class="sr-only">Suivant</span>
+		  </a>
+		</div>
+	</div>
+
+
+	<!--Menton-->
+	<div class="col-md-12" id="slider5">
+		<div id="Menton" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators">
+		    <li data-target="#Menton" data-slide-to="0"></li>
+		    <li data-target="#Menton" data-slide-to="1"></li>
+		    <li data-target="#Menton" data-slide-to="2"></li>
+		    <li data-target="#Menton" data-slide-to="3"></li>
+		    <li data-target="#Menton" data-slide-to="4"></li>
+		    <li data-target="#Menton" data-slide-to="5"></li>
+		    <li data-target="#Menton" data-slide-to="6"></li>
+		    <li data-target="#Menton" data-slide-to="7"></li>
+		  </ol>
+
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner" role="listbox">
+		    <div class="item active">
+		      <img src="img/Menton/M_1.jpg" alt="M_1">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Menton/M_2.jpg" alt="M_2">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Menton/M_3.jpg" alt="M_3">
+		    </div>
+		    <div class="item">
+		      <img src="img/Menton/M_4.jpg" alt="M_4">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Menton/M_5.jpg" alt="M_5">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Menton/M_6.jpg" alt="M_6">
+		    </div>
+		    <div class="item">
+		      <img src="img/Menton/M_7.jpg" alt="M_7">
+		    </div>
+
+		    <div class="item">
+		      <img src="img/Menton/M_8.jpg" alt="M_8">
+		    </div>
+		  </div>
+
+		  <!-- Left and right controls -->
+		  <a class="left carousel-contrM_1ol" href="#Menton" role="button" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		    <span class="sr-only">Precedent</span>
+		  </a>
+		  <a class="right carousel-control" href="#Menton" role="button" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		    <span class="sr-only">Suivant</span>
+		  </a>
+		</div>
+	</div>
+</div>
+
 </div>
 <!-- Fin du contenu -->
 <?php 
