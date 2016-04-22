829671dea40dfa0d3288e13efdda3647512ade5a
Elisaiiie <cayuelaelisa@gmail.com>
2016-04-22 12:43:19 +0200

    MAJ Poitrine

--- web/poitrine/default.php
+++ web/poitrine/default.php
@@ -1,10 +1,44 @@
 <?php
-include('../default/page/header.php');
+include('../default/page/start_header.php');
 ?>
+<!-- Place en dessous ton CSS dans Vendor, et ton CSS local -->
+
+
+<!-- fin CSS -->
+<?php include('../default/page/header.php'); ?>
+
+<!-- Place ici tes scripts Javascripts dans Vendor, et ton JS local -->
+
+
+
+<!-- Fin Scripts -->
+
+
+<?php include('../default/page/end_header.php'); ?>
+
+<!-- Place ici ton contenu -->
+
 <!-- Place en dessous ton contenu + tes liens vers CSS et JS ! -->
+<link href="mambo-poitrine.css" rel="stylesheet" type="text/css" />
+
+
+<div id="container">
 
 
 
+<div class="video">
+    <div class='embed-container'>
+                    <iframe src="https://player.vimeo.com/video/11712103?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
+                    </iframe>
+                </div>
+</div>
+	
+
+
+
+ 
+
+</div>
 
 
 
