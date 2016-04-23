3fa0bccac0443c09e6f5be6c502df848bfb02b86
Oscar G <oscar.goncal@gmail.com>
2016-04-23 11:55:41 +0200

    Maj integration

--- web/gaspacho/default.php
+++ web/gaspacho/default.php
@@ -35,66 +35,66 @@ include( '../default/page/start_header.php' );
 
                 <div class="row-fluid">
 
-                    <div id="container" class="col-sm-5 col-md-5 col-lg-5">
+                    <div id="container" class="col-sm-6 col-md-6 col-lg-6">
 
                         <div class="row-fluid">
-                            <div id="base container" class="col-md-12">
+                            <div id="base container" class="col-md-offset-1 col-md-11">
                                 <h2 style=color:#B54643;>Base</h2>
                                 <h3>(choisir une base)</h3>
 
-                                <div class="col-sm-3 col-md-3 col-lg-3 ingredient" onclick="afficher(this,'base','tomate');"><img
+                                <div class="col-sm-6 col-md-6 col-lg-6 ingredient" onclick="afficher(this,'base','tomate');"><img
                                         id="S37" src="images/tomate_test.png" alt=""/>
                                     <p class="text-center">Tomate</p></div>
-                                <div class="col-sm-3 col-md-3 col-lg-3  ingredient" onclick="afficher(this,'base','potiron');"><img
+                                <div class="col-sm-6 col-md-6 col-lg-6  ingredient" onclick="afficher(this,'base','potiron');"><img
                                         id="S38" src="images/potiron_test.png" alt=""/>
                                     <p class="text-center">Potiron</p></div>
-                                <div class="col-sm-3 col-md-3 col-lg-3 ingredient" onclick="afficher(this,'base','lentille');"><img
+                                <div class="col-sm-6 col-md-6 col-lg-6 ingredient" onclick="afficher(this,'base','lentille');"><img
                                         id="S39" src="images/lentille_test.png" alt=""/>
                                     <p class="text-center">Lentille</p></div>
                             </div>
                         </div>
 
                         <div class="row-fluid">
-                            <div id="legumes container" class="col-md-12">
+                            <div id="legumes container" class="col-md-offset-1 col-md-11">
                                 <h2 style=color:#B54643;>Légumes</h2>
                                 <h3>(choisir deux légumes)</h3>
 
-                                <div class="col-sm-3 col-md-3 col-lg-3  ingredient" onclick="afficher(this,'legumes','concombre');">
+                                <div class="col-sm-6 col-md-6 col-lg-6  ingredient" onclick="afficher(this,'legumes','concombre');">
                                     <img id="S2" src="images/concombre_test.png" alt=""/>
                                     <p class="text-center">Concombre</p></div>
-                                <div class="col-sm-3 col-md-3 col-lg-3  ingredient" onclick="afficher(this,'legumes','poivron');">
+                                <div class="col-sm-6 col-md-6 col-lg-6  ingredient" onclick="afficher(this,'legumes','poivron');">
                                     <img id="S3" src="images/poivron_test.png" alt=""/>
                                     <p class="text-center">Poivron</p></div>
-                                <div class="col-sm-3 col-md-3 col-lg-3  ingredient" onclick="afficher(this,'legumes','chou_fleur');">
+                                <div class="col-sm-6 col-md-6 col-lg-6  ingredient" onclick="afficher(this,'legumes','chou_fleur');">
                                     <img id="S4" src="images/choux_fleur_test.png" alt=""/>
                                     <p class="text-center">Chou-fleur</p></div>
-                                <div class="col-sm-3 col-md-3 col-lg-3 ingredient" onclick="afficher(this,'legumes','poireau');"><img
+                                <div class="col-sm-6 col-md-6 col-lg-6 ingredient" onclick="afficher(this,'legumes','poireau');"><img
                                         id="S5" src="images/poireau_test.png" alt=""/>
                                     <p class="text-center">Poireau</p></div>
                             </div>
                         </div>
 
                         <div class="row-fluid">
-                            <div id="condiments container" class="col-md-12">
+                            <div id="condiments container" class="col-md-offset-1 col-md-11">
                                 <h2 style=color:#B54643;>Condiments</h2>
                                 <h3>(choisir quatres condiments)</h3>
 
-                                <div class="col-sm-3 col-md-3 col-lg-3 ingredient" onclick="afficher(this,'condiments','oignon');">
+                                <div class="col-sm-6 col-md-6 col-lg-6 ingredient" onclick="afficher(this,'condiments','oignon');">
                                     <img id="S9" src="images/oignon_test.png" alt=""/>
                                     <p class="text-center">Oignon</p></div>
-                                <div class="col-sm-3 col-md-3 col-lg-3 ingredient" onclick="afficher(this,'condiments','ail');"><img
+                                <div class="col-sm-6 col-md-6 col-lg-6 ingredient" onclick="afficher(this,'condiments','ail');"><img
                                         id="S10" src="images/ail_test.png" alt=""/>
                                     <p class="text-center">Ail</p></div>
-                                <div class="col-sm-3 col-md-3 col-lg-3 ingredient" onclick="afficher(this,'condiments','basilic');">
+                                <div class="col-sm-6 col-md-6 col-lg-6 ingredient" onclick="afficher(this,'condiments','basilic');">
                                     <img id="S11" src="images/basilic_test.png" alt=""/>
                                     <p class="text-center">Basilic</p></div>
-                                <div class="col-sm-3 col-md-3 col-lg-3 ingredient" onclick="afficher(this,'condiments','piment');">
+                                <div class="col-sm-6 col-md-6 col-lg-6 ingredient" onclick="afficher(this,'condiments','piment');">
                                     <img id="S12" src="images/piment_test.png" alt=""/>
                                     <p class="text-center">Piment</p></div>
-                                <div class="col-sm-3 col-md-3 col-lg-3 ingredient" onclick="afficher(this,'condiments','capres');">
+                                <div class="col-sm-6 col-md-6 col-lg-6 ingredient" onclick="afficher(this,'condiments','capres');">
                                     <img id="S13" src="images/capres_test.png" alt=""/>
                                     <p class="text-center">Capres</p></div>
-                                <div class="col-sm-3 col-md-3 col-lg-3 ingredient"
+                                <div class="col-sm-6 col-md-6 col-lg-6 ingredient"
                                      onclick="afficher(this,'condiments','pois_chiche');">
                                     <img id="S14" src="images/pois_chiche_test.png" alt=""/>
                                     <p class="text-center">Pois-chiche</p>
@@ -104,45 +104,45 @@ include( '../default/page/start_header.php' );
                         </div>
 
                         <div class="row-fluid">
-                            <div id="assaisonement container" class="col-md-12">
+                            <div id="assaisonement container" class="col-md-offset-1 col-md-11">
                                 <h2 style=color:#B54643;>Assaisonnement</h2>
                                 <h3>(choisir quatres assaisonements)</h3>
 
-                                <div class="col-sm-3 col-md-3 col-lg-3 ingredient"
+                                <div class="col-sm-6 col-md-6 col-lg-6 ingredient"
                                      onclick="afficher(this,'assaisonement','citron');"
                                 ><img id="S25"
                                       src="images/citron_test.png"
                                       alt=""/>
                                     <p class="text-center">Citron</p></div>
 
-                                <div class="col-sm-3 col-md-3 col-lg-3 ingredient"
+                                <div class="col-sm-6 col-md-6 col-lg-6 ingredient"
                                      onclick="afficher(this,'assaisonement','vinaigre');"
                                 ><img id="S25"
                                       src="images/vinaigre_test.png"
                                       alt=""/>
                                     <p class="text-center">Vinaigre</p></div>
-                                <div class="col-sm-3 col-md-3 col-lg-3 ingredient"
+                                <div class="col-sm-6 col-md-6 col-lg-6 ingredient"
                                      onclick="afficher(this,'assaisonement','huile_olive');">
                                     <img id="S26"
                                          src="images/huile_olive_test.png"
                                          alt=""/>
                                     <p class="text-center">Huile d'olive</p></div>
-                                <div class="col-sm-3 col-md-3 col-lg-3 ingredient"
+                                <div class="col-sm-6 col-md-6 col-lg-6 ingredient"
                                      onclick="afficher(this,'assaisonement','poivre');">
                                     <img id="S27" src="images/poivre_test.png"
                                          alt=""/>
                                     <p class="text-center">Poivre</p></div>
-                                <div class="col-sm-3 col-md-3 col-lg-3 ingredient" onclick="afficher(this,'assaisonement','sel');">
+                                <div class="col-sm-6 col-md-6 col-lg-6 ingredient" onclick="afficher(this,'assaisonement','sel');">
                                     <img id="S28" src="images/sel_test.png" alt=""/>
                                     <p class="text-center">Sel</p></div>
-                                <div class="col-sm-3 col-md-3 col-lg-3 ingredient" onclick="afficher(this,'assaisonement','curry');">
+                                <div class="col-sm-6 col-md-6 col-lg-6 ingredient" onclick="afficher(this,'assaisonement','curry');">
                                     <img id="S29" src="images/curry_test.png" alt=""/>
                                     <p class="text-center">Curry</p></div>
                             </div>
                         </div>
 
                         <div class="row-fluid">
-                            <div id="temperature container" class="col-md-12">
+                            <div id="temperature container" class="col-md-offset-1 col-md-11">
                                 <h2 style=color:#B54643;>Température</h2>
                                 <h3>(un choix possible)</h3>
 
@@ -189,9 +189,18 @@ include( '../default/page/start_header.php' );
         ERREUR RECETTE !
     </div>
     <div style="display:none" id="valid">
-        WELL DONE !
-    </div>
-    <!-- Fin du contenu -->
+        <span>Félicitations votre gaspacho semble plaire à Henry !</span>
+    </br>
+
+<object type="application/x-shockwave-flash" width="540" height="355" data="http://www.youtube.com/v/_etwz7NkemE&hl=fr">
+        <param name="movie" value="http://www.youtube.com/v/_etwz7NkemE&hl=fr" />
+        <param name="wmode" value="transparent" />
+    </object>
+
+<!--[if lte IE 6 ]>
+    <embed src="http://www.youtube.com/v/_etwz7NkemE&hl=fr" type="application/x-shockwave-flash" wmode="transparent" width="425" height="355"></embed>
+<![endif]-->
+
 <?php
 include( '../default/page/footer.php' );
 ?>
