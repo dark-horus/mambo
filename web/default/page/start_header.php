4b624eb92fb0acf507df59d979f2cff29d12807e
MarlenePino <Marlene@macbook-pro-de-marlene.home>
2016-04-23 14:22:39 +0200

    Consignes 'Mambo Canaille'
    -Dans les consignes, placement du texte censé être en italique entre
    ‘…’ en attendant de trouver la solution.
    -Titre avec de l’italique fonctionne.

--- web/default/page/start_header.php
+++ web/default/page/start_header.php
@@ -46,7 +46,7 @@ if ($dossier != null) {
 			$next = '<li class="col-md-12"><a href="../immeuble">Étape suivante : immeuble</a></li>';
 			$before = '<li class="col-md-12"><a href="../livre">Étape précédente : livre </a></li>';
 			$current = '<li class="col-md-12"><a href="#" >Étape en cours : '.$dossier.'</a></li>';
-            $consigne ="Vous n'êtes ni Christophe Colomb ni au 15ème siècle, mais explorez le vaste monde de l'art culinaire de Mambo Canaille et mettez la main à la pâte en ajoutant votre recette à ce délicieux planisphère.";
+            $consigne ="Vous n'êtes ni Christophe Colomb ni au 15ème siècle, mais explorez le vaste monde de l'art culinaire de 'Mambo Canaille' et mettez la main à la pâte en ajoutant votre recette à ce délicieux planisphère.";
 			break;
         case "vin":
             $titre = "Initiez-vous à l’oenologie chilienne";
