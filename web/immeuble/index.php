fceec1e99beaf5095cdb699927fdd7e4dd49a7d9
Artthed <arthur.desoutter@outlook.com>
2016-04-24 20:40:18 +0200

    immeuble

--- web/immeuble/index.php
+++ web/immeuble/index.php
@@ -3,7 +3,7 @@ include('../default/page/start_header.php');
 ?>
 <!-- Place en dessous ton CSS dans Vendor, et ton CSS local -->
 
-
+<link rel="stylesheet" type="text/css" href="style.css">
 <!-- fin CSS -->
 <?php include('../default/page/header.php'); ?>
 
@@ -18,10 +18,11 @@ include('../default/page/start_header.php');
 
 <!-- Place ici ton contenu -->
 
+<img class="img-responsive" src="images/couloir.png" width="100%">
 
-
-
-
+<a href="chambre1.php"><div id="porte1"></div></a>
+<a href="chambre2.php"><div id="porte2"></div></a>
+<a href="chambre3.php"><div id="porte3"></div></a>
 <!-- Fin du contenu -->
 <?php 
 include('../default/page/footer.php');
