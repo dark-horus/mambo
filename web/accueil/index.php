5a1d503c141faf5d2d7574a1f9354558fd9e4c36
kieranpaccoud <kieran.paccoud@gmail.com>
2016-04-26 12:18:07 +0200

    Debut Maj Css / + rename Index.php

--- web/accueil/index.php
+++ web/accueil/index.php
@@ -35,15 +35,15 @@ include('../default/page/start_header.php');
 
 <!-- Place ici ton contenu -->
  <!-- Set your background image for this header on the line below. -->
- <div id="haut_page" class="col-lg-12">
+ <div id="haut_page" class="col-lg-12 col-md-12 col-xs-12">
     <!-- Main Content -->
     <a class="fancybox" data-fancybox-type="iframe" href="../default/page/index.php"></a>
 
         <div id="top_site"class="row">
-            <div class="col-lg-6 col-lg-offset-0 col-md-6 col-md-offset-0">
+            <div class="col-lg-5 col-md-5 col-xs-12">
             <iframe class="embed-responsive-item" src="https://player.vimeo.com/video/123630710?color=1da7bf" width="720" height="520" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
             </div>
-            <div class="col-lg-4 col-lg-offset-0 col-md-6 col-md-offset-0">
+            <div class="col-lg-4 col-md-4 col-md-offset-2 col-xs-12">
                 <p>Vous êtes en train de lire <em>Mambo Canaille</em> ? <br/> Parcourez au fil d’icônes l’univers         fantasque de Juan Carlos Mendez Guédez, déambulez dans les rues de Madrid, créez vos recettes, devenez à votre tour écrivain, évadez-vous au delà des frontières du livre...</p>
             </div>
         </div>
