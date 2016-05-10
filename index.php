aef8c311ba470561f06a3c9694eb757d24b2d039
Artthed <arthur.desoutter@outlook.com>
2016-05-10 15:37:18 +0200

    index redirection

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0;url=web/accueil/index.php">
+<title><?php echo['ID']; ?></title>
+<script language="javascript">
+    window.location.href = "web/accueil/index.php"
+</script>
+</head>
+<body>
+</body>
+</html>
