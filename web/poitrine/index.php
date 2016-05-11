c8f43d3a20b3e34ae1a0e6f496558ca9c4a14423
Elisaiiie <cayuelaelisa@gmail.com>
2016-05-11 16:32:39 +0200

    Poitrine : lien vidéo + titre

--- web/poitrine/index.php
+++ web/poitrine/index.php
@@ -20,6 +20,7 @@ include('../default/page/start_header.php');
 
 <!-- Place en dessous ton contenu + tes liens vers CSS et JS ! -->
 <link href="mambo-poitrine.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="../default/page/CSS/font.css">
 
 
 <div id="container">
@@ -28,7 +29,9 @@ include('../default/page/start_header.php');
 
 <div class="video">
     <div class='embed-container'>
-                    <iframe src="https://player.vimeo.com/video/11712103?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
+                    <iframe src="https://www.youtube.com/embed/8sOMBuEDeB4" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
+                    
+
                     </iframe>
                 </div>
 </div>
