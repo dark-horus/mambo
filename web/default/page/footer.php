ef57878f16e8779af1a61437d6edfeaa15bb39e4
Artthed <arthur.desoutter@outlook.com>
2016-05-11 14:31:36 +0200

    jquery ui pour ordi

--- web/default/page/footer.php
+++ web/default/page/footer.php
@@ -1,6 +1,6 @@
 </div>
 <script src="../../vendor/jquery-2.2.1/jquery-2.2.1.min.js"></script>
-
+<script src="../../vendor/jquery-ui/jquery-ui.js"></script>
 <script>
     $(document).ready(function () {
             if ($(".fancybox").length) {
@@ -10,7 +10,7 @@
         }
     );
 	</script>
-	<script type="text/javascript" src="../../vendor/fancybox/source/jquery.fancybox.js"></script>
+<script type="text/javascript" src="../../vendor/fancybox/source/jquery.fancybox.js"></script>
 <script type="text/javascript" src="../../vendor/fancybox/source/jquery.fancybox.pack.js"></script>
 <script type="text/javascript" src="../../vendor/fancybox/source/jquery.mousewhell.js"></script>
 <script src="../../vendor/bootstrap/js/bootstrap.js" type="text/javascript"></script>
