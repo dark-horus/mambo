544d8ba8385c942f94771c363c2b3c6b41d90f9b
MarlenePino <marlene.pineaulebrun@gmail.com>
2016-05-12 10:59:49 +0200

    Hébergeur

--- web/mentions/mentionslegales.php
+++ web/mentions/mentionslegales.php
@@ -43,28 +43,27 @@ p {
 <div id='mentionslegales'>
     
 <h2><center>Les mentions légales</center></h2>
-
-<h3>Les informations légales</h3>
+<h3><b>Les informations légales</b></h3>
 <p>Vous êtes en train de visiter www.mambocanaille.fr, le site de la maison d’édition Zinnia Editions basé sur le livre « Chulapos Mambo » (<i>Mambo Canaille</i> en français) écrit par Juan Carlos Méndez Guédez. <br/> Ce site original et novateur a été conçu et réalisé par les étudiants du Master Audiovisuel et Médias Numériques (AVMN), parcours Communication Multimédia Interactif de l’Université Grenoble Alpes, promotion 2015-2017.</p>
 
-<h3>Les responsabilités et contacts</h3>
+<h3><b>Les responsabilités et contacts</b></h3>
 <p>Directeur de la publication : Magali Homps</p>
 
-<h3>L’accessibilité</h3>
+<h3><b>L’accessibilité</b></h3>
 <p>Le site www.mambocanaille.fr est gratuit et par principe accessible aux utilisateurs 24/24h, 7/7j, sauf interruption, programmée ou non, pour les besoins de sa maintenance ou en cas de force majeure. L’accès et l’utilisation de ce site sont soumis aux dispositions légales en vigueur et s’appliquent à tout internaute visitant ce site. <br/>
 En accédant à ce site, vous acceptez, sans limitation ni réserve, ces conditions.</p>
 
-<h3>Le contenu</h3>
+<h3><b>Le contenu</b></h3>
 <p>Les informations et les vidéos contenues sur ce site sont destinées à promouvoir la maison d’éditions Zinnia et plus particulièrement, à partager au plus grand nombre un contenu de qualité. </br>
 Le directeur de la publication ne s’engage pas à une novation régulière de ce site internet et n’est pas responsable de l’utilisation et de l’interprétation de l’information qu’il contient. </br>
 Si des informations sont incorrectes, non mises à jour ou pour signaler un contenu inapproprié ou illégal, vous pouvez écrire à l’adresse suivante : moderation.mambocanaille@gmail.com</p>
 
-<h3>La protection des matériels</h3>
+<h3><b>La protection des matériels</b></h3>
 <p>Le directeur de publication ne saurait être tenu pour responsable d’éventuels dommages, directs ou indirects, écoulant de votre accès ou utilisation de ce site. </br>
 De plus, il ne saurait être tenu responsable d’un dommage ou virus qui pourrait infecter votre ordinateur ou tout matériel informatique, suite à une utilisation ou accès au site ou téléchargement provenant de ce site. </br>
 Des liens vers d’autres sites peuvent vous faire sortir de la plateforme, le directeur de la publication n’accepte aucune responsabilité sur les sites pointés. Ces liens sont établis de bonne foi et il ne saurait être tenu pour responsable de changements sur ces sites.</p>
 
-<h3>Les droit d’auteur et avertissement légal aux utilisateurs</h3>
+<h3><b>Les droit d’auteur et avertissement légal aux utilisateurs</b></h3>
 <p>Le présent site constitue une œuvre multimédia originale dont les étudiants du Master Audiovisuel et Médias Numériques, parcours Communication Multimédia Interactif, promotion 2015-2017 (mentionnés dans les crédits du site), sont les auteurs, au sens des articles L.111.1 et suivants du Code de la Propriété Intellectuelle : </br>- Cyril Alcalay</br>- Elisa Cayuela</br>- Arthur Desoutter</br>- Annabel Gazzano</br>- Oscar Goncalves</br>- Alizée Letellier</br>- Kieran Paccoud</br>- Marlène Pineau</br>- Luc Pré</br>- Kim Sauermost-Spratt</br>- Romane Viard</br></p>
 <p>Pour toute capture d’écran, ou référence au site, veuillez utiliser la mention : Master AVMN / Mambo Canaille. Les captations sonores, les vidéos ainsi que les photos du site www.mambocanaille.fr constituent des oeuvres dont les étudiants du Master Audiovisuel et Médias Numériques, parcours Multimédia Interactif sont les auteurs :</p>
 <p><u>L’envolée du Gaspacho :</u></br>- Alizée Letellier</br>- Kim Sauermost-Spratt</br>- Romane Viard</br></p><p><u>Les femmes d’hier et d’aujourd’hui :</u></br>- Elisa Cayuela</br>- Annabel Gazzano</br>- Marlène Pineau</br></p><p><u>Photographies de l’étape Chien :</u></br>- Cyril Alcalay</p>
@@ -72,25 +71,31 @@ Des liens vers d’autres sites peuvent vous faire sortir de la plateforme, le d
 <p>Pour toutes utilisations et exploitations des photographies, vidéos et sons, veuillez utiliser le crédit : Master AVMN / Mambo Canaille / Zinnia Editions.</p>
 <p>Pour réaliser le site internet www.mambocanaille.fr nous utilisons différents plugins : </br>- Pure CSS interactive map </br>- Libre de droit. http://codepen.io/scarl3tt/pen/jEBmyG</br>- MasterMind Game Module - Copyright 2016 by Neil McCallion. L’avis de copyright et l’avis d’autorisation doivent être inclus dans toutes les copies ou parties substantielles du logiciel. http://codepen.io/njmcode/pen/Myapmr</br>- JInvertScroll </br>- Défilement horizontal (Étape Ferrari). Copyright (c) 2013 pixxelfactory. L’avis du copyright et d’autorisation devront être inclus dans le dispositif final. http://www.pixxelfactory.net/jInvertScroll/license.php</p>
 
-<h3>Les conditions d’utilisation</h3>
+<h3><b>Les conditions d’utilisation</b></h3>
 <p>Toute utilisation par reproduction partielle ou totale, modification, diffusion ou autre procédé existant présent ou futur, à des fins commerciales, promotionnelles et/ou publicitaires est interdite, de quelques manières que ce soit et sur quelque support que ce soit, sans l’autorisation expresse du directeur de publication Magali Homps et des auteurs concernés.</br> Seules les copies sans modification, à usage privé et non commercial sont autorisées sous réserve de la mention : Master AVMN/Mambo Canaille/Zinnia Editions, dans le respect des droits de propriété intellectuelle. Le site et ses contenus sont ainsi soumis à la Licence Creative Commons BY-NC-ND.</br>L’absence d’accord représente un délit de contrefaçon, conformément aux dispositions de l’article L122-5 du Code de la Propriété Intellectuelle.</p>
 
-<h3>La configuration informatique minimum</h3>
+<h3><b>La configuration informatique minimum</b></h3>
 <p>L’utilisateur du site www.mambocanaille.fr reconnaît disposer de la compétence et des moyens nécessaires pour utiliser ce site.</br> L’accès au site et son utilisation nécessitent de disposer de la configuration minimale suivante : un ordinateur (Mac ou PC) équipé d’un navigateur (Google Chrome, Firefox, Safari, ou Opera), JavaScript activé. </br> Ce site est optimisé pour une résolution de 1024 pixels et a été testé avec succès sur Mac, Linux et Windows, en utilisant les navigateurs Chrome 41, Firefox 37, Safari 8, ou Opera 28 et supérieurs.</p>
 
-<h3>L’hébergement et réalisation du site</h3>
-<p>Ce site est hébergé au nom du directeur de publication (Magali Homps) par les maisons d’édition Zinnia. Les informations et services proposés par le site web www.mambocanaille.fr.</p>
+<h3><b>L’hébergement et réalisation du site</b></h3>
+<p>Ce site est hébergé au nom du directeur de publication (Magali Homps) des éditions Zinnia. Les informations et services proposés par le site web www.mambocanaille.fr.</p>
 <p>Institut de la Communication et des Médias</br>
 Université Grenoble Alpes</br>
 11, avenue du 8 mai 1945 - BP 337</br>
 38434 Échirolles Cedex</p>
+<p><u>Hébergeur :</u></br>
+<b>OVH</b></br>
+140, Quai du Sartel</br>
+59100 ROUBAIX</br>
+FRANCE</br>
+</p>
 
 <h2><center>Les conditions générales d'utilisation</center></h2>
-<h3>L’objet</h3>
+<h3><b>L’objet</b></h3>
 <p>Les présentes Conditions Générales ont pour objet de définir les modalités de mise à disposition des services du site mambo canaille.</br>
 Tout accès et/ou utilisation du site mambo canaille suppose l’acceptation et le respect de l’ensemble des termes des présentes Conditions et leur acceptation inconditionnelle. Elles constituent donc un contrat entre le service et l’utilisateur.</br>Dans le cas où l’utilisateur ne souhaite pas accepter tout ou partie des présentes conditions générales, il lui est demandé de renoncer à tout usage du service.</p>
 
-<h3>La protection des données à caractère personnel</h3>
+<h3><b>La protection des données à caractère personnel</b></h3>
 <p>Certains espaces et services de www.mambocanaille.fr sont en accès réservés exclusivement aux utilisateurs disposant d’un compte. Le mot de passe est nécessaire pour accéder à certaine étapes, il est délivré par www.mambocanaille.fr.</br>
 Le mot de passe est personnel et strictement confidentiel. A ce titre, l’utilisateur doit s’en réserver l’usage exclusif. L’utilisateur est seul responsable des conséquences qui pourraient résulter de leur communication à des tiers. L’utilisateur s’engage à informer immédiatement les responsables du site www.mambocanaille.fr de toute utilisation non autorisée de ses clés de connexion, et plus généralement de toute atteinte à la sécurité du site www.mambocanaille.fr dont il aurait connaissance.</br>
 L’utilisateur du site www.mambocanaille.fr s’interdit de transmettre tout contenu contraire à une loi en vigueur. L’utilisateur s’interdit également de transmettre toute publicité ou matériel non sollicités (« spamming »), ainsi que tout contenu à caractère diffamatoire, raciste, pornographique ou comportant des virus informatiques.</br></p>
@@ -100,24 +105,24 @@ Les maisons d’éditions Zinnia prennent toutes les dispositions visant à as
 Le responsable de traitement des données collectées sur le site www.mambocanaille.fr est Zinnia Éditions. Seul, le personnel de Zinnia Éditions et de ses  filiales habilité dans l’exercice de ses fonctions en charge du traitement des demandes, est destinataire des données personnelles que vous communiquez via le site www.mambocanaille.fr.</br>
 Les données personnelles que vous communiquez sur le site www.mambocanaille.fr peuvent être utilisées à des fins statistiques sur la fréquentation du site mais ne seront jamais utilisées à des fins commerciales.</p>
 
-<h3>La durée de conservation des données à caractère personnel</h3>
+<h3><b>La durée de conservation des données à caractère personnel</b></h3>
 <p>Les données à caractère personnel que l’utilisateur communique sur les sites www.mambocanaille.fr sont collectées, traitées et conservées par Zinnia Éditions dans le respect de la loi n°78 -17 du 6 janvier 1978 modifiée le 6 août 2004 relative à l’informatique et aux libertés pour une durée non limitée. Liens hypertextes pointant, depuis le site www.mambocanaille.fr, vers d’autres sites.</p>
 
-<h3>La gestion des contenus</h3>
+<h3><b>La gestion des contenus</b></h3>
 <p>En publiant et créant du contenu sur le site www.mambocanaille.fr (notamment dans l’étape “Livre”) l’internaute cède en totalité et de manière incontestable ses droits d’auteur. Tout contenu créé, modifié, ajouté et publié sur le site www.mambocanaille.fr appartient à Zinnia Éditions. Zinnia Éditions se réserve le droit de modifier, modérer et diffuser ces contenus librement et dans un but non commercial.</p>
 
-<h3>Les droit d’accès, de modification, de rectification, de suppression et d’opposition</h3>
+<h3><b>Les droit d’accès, de modification, de rectification, de suppression et d’opposition</b></h3>
 <p>Conformément à la loi susvisée, vous disposez d’un droit d’accès, d’interrogation, de modification, de rectification et d’opposition permanent aux informations qui vous concernent.</br>Si vous souhaitez exercer ce droit, il vous suffit de nous contacter en joignant à votre demande la copie de votre pièce d’identité :</br></p>
 
 <p>Par courrier postal : Zinnia Éditions - 168 RUE CUVIER - 69006 LYON</br>
 Par courrier électronique à : zinnia.editions@gmail.com</p>
 
-<h3>Les responsabilités du site internet</h3>
+<h3><b>Les responsabilités du site internet</b></h3>
 <p>Les maisons d’éditions Zinnia déclinent toute responsabilité :
 En cas d’interruption du site www.mambocanaille.fr pour des opérations de maintenance techniques ou d’actualisation des informations publiées, en cas d’impossibilité momentanée d’accès au site internet en raison de problèmes techniques et ce quelles qu’en soient l’origine et la provenance.</br> En cas de dommages directs ou indirects causés à l’utilisateur, quelle qu’en soit la nature, résultant du contenu, de l’accès, ou de l’utilisation du site www.mambocanaille.fr (et/ou des sites qui lui sont liés).</br>
 En cas d’utilisation anormale ou d’une exploitation illicite du site www.mambocanaille.fr, l’utilisateur du site www.mambocanaille.fr est alors seul responsable des dommages causés aux tiers et des conséquences des réclamations ou actions qui pourraient en découler. L’utilisateur renonce également à exercer tout recours contre les maisons d’édition Zinnia dans le cas de poursuites diligentées par un tiers à son encontre du fait de l’utilisation et/ou de l’exploitation illicite du site.</p>
 
-<h3>La charte d’utilisation</h3>
+<h3><b>La charte d’utilisation</b></h3>
 <p><u>Avant-propos</u></br>
 Cette charte a été élaborée afin de préciser aux utilisateurs du site www.mambocanaille.fr les conditions d’utilisation de ce dernier.</br>
 Toute personne naviguant sur le site est considérée comme un utilisateur, qu’elle soit identifiée ou non sur le site.</br>
@@ -140,7 +145,7 @@ Les maisons d’Éditions Zinnia se réserve le droit à tout moment de :</br
 - Mettre  fin à tout ou partie du droit d’accès, supprimer le compte et mot de passe, retirer tout contenu diffusé et résilier, de plein droit et sans indemnité ni préavis, toute adhésion concernant tout utilisateur n’ayant pas respecté l’une quelconque des dispositions de la présente charte.</br>
 - Changer tout ou partie des identifiants notamment pour des raisons d’ordre technique ou de sécurité sans que l’utilisateur puisse réclamer une quelconque indemnité.</p>
 
-<h3>Les crédits et les remerciements</h3>
+<h3><b>Les crédits et les remerciements</b></h3>
 <p>Découvrez www.mambocanaille.fr, le site internet des maisons d’éditions Zinnia qui propose une lecture augmentée du livre <i>Mambo Canaille</i> de Juan Carlos Mendéz Guedéz. Parcourez au  fil d’icônes l’univers fantasque de l’auteur. Déambulez dans les rues de Madrid, créez vos recettes, devenez à votre tour écrivain, évadez-vous au-delà des frontières du livre. Plongez avec Henry, Simao et Alejandro dans un voyage des plus croustillants ! Bonne visite !</br></p>
 <p>Pour la conception et la réalisation ainsi que pour le contenu vidéo, photo et audio du site Mambo Canaille : Cyril Alcalay, Elisa Cayuela, Arthur Desoutter, Annabel Gazzano, Oscar Goncalves, Alizée Letellier, Kieran Paccoud, Marlène Pineau, Luc Pré, Romane Viard, Kim Sauermost-Spratt.</p>
 <p>Nous remercions, Franck Grimonpont ; Stéphane Koeberlé ; Laurence Payan ; Laurie Schmitt ; Tommy Redolfi ; Françoise Papa.
