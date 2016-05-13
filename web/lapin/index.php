92f261bd7f73d3a2d976bb10b99965e74cbddd68
kieranpaccoud <kieran.paccoud@gmail.com>
2016-05-13 16:21:12 +0200

    Changes_Lapin

--- web/lapin/index.php
+++ web/lapin/index.php
@@ -16,34 +16,17 @@
             
        <!-- <h1>Serez-vous le nouveau conquistador culinaire de mambo canaille ?</h1>-->
             
-               <a class="fancybox" data-fancybox-type="iframe" href="img/recette_gaspa.jpg" id="recette">
-                    <div class="marker" id="m1">
-
-                    </div>
-                </a>
-                <a class="fancybox" data-fancybox-type="iframe" href="img/recette_tapas.jpg" id="recette">
-                    <div class="marker"id="m2">
-
-                    </div>
-                </a>
-                <a class="fancybox" data-fancybox-type="iframe" href="img/recette_tortilla.jpg" id="recette">    
-                    <div class="marker"id="m3">
-
-                    </div>
-                </a>
-                <a class="fancybox" data-fancybox-type="iframe" href="img/recette_potaje.jpg" id="recette">
-                    <div class="marker"id="m4">
-
-                     </div>
-                </a>
-
-                <a class="fancybox" data-fancybox-type="iframe" href="img/recette_lapin.jpg" id="recette">
-                    <div class="marker"id="m5">
-
-                     </div>
-                </a>
-
-         
-
+            <img src="./img/backgfinal.jpg" alt="" usemap="#Map" />
+<map name="Map" id="Map">
+    <area class="fancybox" data-fancybox-type="iframe" href="img/recette_lapin.jpg" id="recette" shape="poly" coords="630,309,574,373,637,478,691,357" />
+    <area class="fancybox" data-fancybox-type="iframe" href="img/recette_potaje.jpg" id="recette" shape="poly" coords="786,404,706,467,785,563,851,437" />
+    <area class="fancybox" data-fancybox-type="iframe" href="img/recette_tapas.jpg" id="recette" shape="poly" coords="692,646,629,726,690,821,757,716" />
+    <area class="fancybox" data-fancybox-type="iframe" href="img/recette_tortilla.jpg" id="recette" shape="poly" coords="480,400,414,454,475,578,538,457" />
+    <area class="fancybox" data-fancybox-type="iframe" href="img/recette_gaspa.jpg" id="recette" shape="poly" coords="1231,237,1162,297,1240,420,1299,299" />
+</map>
         </body>
+        <!-- Fin du contenu -->
+<?php 
+include('../default/page/footer.php');
+?>
     </html>
