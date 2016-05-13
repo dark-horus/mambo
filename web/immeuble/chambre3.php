57549e5f10bbed0a3ddd607c8243ab864a024448
ArtheD <arthur.desoutter@outlook.com>
2016-05-13 15:47:58 +0200

    immeuble update

--- web/immeuble/chambre3.php
+++ web/immeuble/chambre3.php
@@ -35,84 +35,113 @@ while($values = $step->fetch()){
 <div id="image">
 <img src="images/chambre3.png" width="1920" height="1080" usemap="#maparthur">
  <map name="maparthur" id="maparthur">
-		<area shape="rect" title="livre" coords="2,804,96,850" href="#" onclick="showElem()"/>  		
-        <area shape="rect" coords="1263,944,1298,960" href="#" title="appareil" onclick="showElem2()"/>
-     	<area shape="rect" coords="1846,746,1888,767" href="#" title="Stylo" onclick="showElem3()"/>
-
+		<area shape="rect" title="vin" coords="-2,862,56,1022" href="#" alt="vin" onclick="showElem()"/>  		
+        <area shape="rect" coords="473,750,566,814" href="#" title="os" alt="os" onclick="showElem2()"/>
+     	<area shape="rect" coords="708,796,758,846" href="#" title="telephone" alt="telephone" onclick="showElem3()"/>
+        <area shape="rect" coords="962,640,1050,676" href="#" title="journal" alt="journal" onclick="showElem4()"/>
+     	<area shape="rect" coords="1612,560,1684,784" href="#" title="batte" alt="batte" onclick="showElem5()"/>
  </map>
 </div>
- <div id="obj">
+ <div id="obj" class="col-md-4">
  <div class="col-md-12">
  <h2>Objets trouvés :</h2>
+ <br>
  </div>
- <ul class="list-inline col-md-12">
-	<li id="livre" style="visibility:hidden;"><img class="img-responsive" src="images/livre.png"></li>
-	<li id="appareil" style="visibility:hidden;"><img class="img-responsive" src="images/livre.png"></li>
-	<li id="stylo" style="visibility:hidden;"><img class="img-responsive" src="images/livre.png"></li>
+ <ul class="list-inline">
+	<li id="vin" style="visibility:hidden;"><img width="50px" height="50px" class="img-responsive" src="images/vin.png"></li>
+	<li id="os" style="visibility:hidden;"><img width="50px" height="50px" class="img-responsive" src="images/os.png"></li>
+	<li id="telephone" style="visibility:hidden;"><img width="50px" height="50px" class="img-responsive" src="images/telephone.png"></li>
+ 	<li id="journal" style="visibility:hidden;"><img width="50px" height="50px" class="img-responsive" src="images/journal.png"></li>
+	<li id="batte" style="visibility:hidden;"><img width="50px" height="50px" class="img-responsive" src="images/batte.png"></li>
  </ul>
    </div>
  
- <div id="question" style="visibility:hidden">
+ <div id="question" class="col-md-4" style="visibility:hidden">
+ <div class="col-md-12">
  <h2>La chambre appartient à :</h2>
+ </div>
  <form action="verif.php" method="post" name="param" onsubmit="return confirmation()">
-
+<div class="col-md-12">
 <input type="radio" name="chambre3" id="r1" value="Alejandro" onclick="reponse()">
 Alejandro
 <input type="radio" name="chambre3" id="r2" value="Henry" onclick="reponse()">
 Henry
 <input type="radio" name="chambre3" id="r3" value="Simao" onclick="reponse()">
 Simao
-<input type="submit" name="valider" id="valider" value="Valider l'étape">
+</div>
+<input type="submit" name="valider" id="valider" class="mmmvalider" value="">
 
 </form>
 </div>
    
-   <div id="portes">
+   <div id="portes" class="col-md-4">
    <h2>Changer de chambre :</h2>
 	<ul class="list-inline col-md-12">
 	<li class="col-md-2"><div class="img-thumbnail"><a href="chambre1.php"><img class="col-md-12" width="50px" height="60px" src="images/porte1.png" alt="chambre1" title="chambre1"><div class="col-md-12"><?php if(isset($henry)){ echo $henry ; } ?></div></a></div></li>
-	<li class="col-md-2"><div class="img-thumbnail"><a href="chambre2.php"><img class="col-md-12" width="50px" height="60px" src="images/porte2.png" alt="chambre2" title="chambre2"><div class="col-md-12"><?php  if(isset($alejandro)){ echo $alejandro ; } ?></div></a></li>
-	<li class="col-md-2"><div class="img-thumbnail"><a href="chambre3.php"><img class="col-md-12" width="50px" height="60px" src="images/porte3.png" alt="chambre3" title="chambre3"><div class="col-md-12"><?php  if(isset($simao)){ echo $simao ;} ?></div></a></li>
+	<li class="col-md-2"><div class="img-thumbnail"><a href="chambre2.php"><img class="col-md-12" width="50px" height="60px" src="images/porte2.png" alt="chambre2" title="chambre2"><div class="col-md-12"><?php  if(isset($alejandro)){ echo $alejandro ; } ?></div></a></div></li>
+	<li class="col-md-2"><div class="img-thumbnail"><a href="chambre3.php"><img class="col-md-12" width="50px" height="60px" src="images/porte3.png" alt="chambre3" title="chambre3"><div class="col-md-12"><?php  if(isset($simao)){ echo $simao ;} ?></div></a></div></li>
  </ul>
   </div>  
-   
+   </div>
 <script>
 	var count = 0;
 	var count1 = 0;
 	var count2 = 0;
+	var count3 = 0;
+	var count4 = 0;
 
 function showElem() {
-    document.getElementById("livre").style.visibility = "visible";
+    document.getElementById("vin").style.visibility = "visible";
 	if(count == 0){
 		count = 1;
-		if(count + count1 + count2 > 2 )
+		if(count + count1 + count2 + count3 + count4 > 2 )
 {  
         document.getElementById("question").style.visibility = "visible";     
 }	
 	}
 }
 function showElem2() {
-    document.getElementById("appareil").style.visibility = "visible";
+    document.getElementById("os").style.visibility = "visible";
 	if(count1 == 0){
 		count1 = 1;
-		if(count + count1 + count2 > 2 )
+		if(count + count1 + count2 + count3 + count4 > 2 )
 {  
         document.getElementById("question").style.visibility = "visible";     
 }	
 	}
 }
 function showElem3() {
-    document.getElementById("stylo").style.visibility = "visible"; 
+    document.getElementById("telephone").style.visibility = "visible"; 
 	if(count2 == 0){
 		count2 = 1;
-		if(count + count1 + count2 > 2 )
+		if(count + count1 + count2 + count3 + count4 > 2 )
 {  
         document.getElementById("question").style.visibility = "visible";     
 }	
 
 	}
 }
+function showElem4() {
+    document.getElementById("journal").style.visibility = "visible"; 
+	if(count3 == 0){
+		count3 = 1;
+		if(count + count1 + count2 + count3 + count4 > 2 )
+{  
+        document.getElementById("question").style.visibility = "visible";     
+}	
 
+	}
+}function showElem5() {
+    document.getElementById("batte").style.visibility = "visible"; 
+	if(count4 == 0){
+		count4 = 1;
+		if(count + count1 + count2 + count3 + count4 > 2 )
+{  
+        document.getElementById("question").style.visibility = "visible";     
+}	
+
+	}
+}
 
 </script>
 <?php 
