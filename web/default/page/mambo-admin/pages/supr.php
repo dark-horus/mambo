93aa6f22e7301220e0970b0677eb5c91f7f24a36
Artthed <arthur.desoutter@outlook.com>
2016-05-13 10:08:38 +0200

    Correction suppression user

--- web/default/page/mambo-admin/pages/supr.php
+++ web/default/page/mambo-admin/pages/supr.php
@@ -1,11 +1,19 @@
 <?php
 
+ini_set("display_errors",1);
+ini_set("error_reporting",E_ALL^E_NOTICE);
+
 session_start();
 include('bdd.php');
 
 $id = $_GET["user"];
-	//echo ($id);
-	$bdd->query("DELETE FROM `user` WHERE iduser ='$id'");
-
+echo "DELETE FROM `user` WHERE `iduser` = '$id'".'<br>';
+	echo $id;
+	try {
+		$bdd->query("DELETE FROM `etape` WHERE `user_ID` = '$id'");
+		$bdd->query("DELETE FROM `user` WHERE `iduser` = '$id'");
+	} catch(Exception $e) {
+		echo $e->getMessage();
+	}
 	header('location: tables.php');
 ?>
