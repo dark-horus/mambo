f115fc152466040ad7b5432673f89b1fc3f0ac1c
ArtheD <arthur.desoutter@outlook.com>
2016-05-16 15:52:00 +0200

    correction admin

--- web/default/page/mambo-admin/pages/index.php
+++ web/default/page/mambo-admin/pages/index.php
@@ -82,7 +82,7 @@ $notifChien = $bdd->query($notifChien)->fetchColumn();
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php">>Bonjour <?php echo $administr['login']; ?></a>
+                <a class="navbar-brand" href="index.php">Bonjour <?php echo $administr['login']; ?></a>
             </div>
             <!-- /.navbar-header -->
 
