a2a10bc7ec09e02629418687d6152fce4efff54c
Luc <luc.pre@outlook.fr>
2016-05-18 17:45:19 +0200

    favicon et title

--- web/default/page/header.php
+++ web/default/page/header.php
@@ -3,3 +3,5 @@
 <script src="../../vendor/jquery-2.2.1/jquery-2.2.1.min.js"></script>
 <script src="../../vendor/jquery-2.2.1/jquery-migrate-1.2.1.min.js"></script>
 <script type="text/javascript" src="../../vendor/rwdImageMaps/jquery.rwdImageMaps.js"></script>
+<link rel="icon" type="image/jpeg" href="favicon.png" />
+<title>Mambo Canaille</title>
